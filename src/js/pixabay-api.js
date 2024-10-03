e6e5bc8f8870d5dae3e2c31aba5c528ba276820d
KonstantinStetsenko <160874184+KonstantinStetsenko@users.noreply.github.com>
2024-10-04 02:24:15 +0300

    v9

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,16 +1,15 @@
 import axios from 'axios';
 import { search } from '../main';
-import { _page, _per_page } from '../main';
+import { _page } from '../main';
 import { refs } from '../main';
-
-const API_KEY = '35615577-2bf6ed1d70f9a1c328f0e4a49';
-const BASE_URL = 'https://pixabay.com/api/';
-
 export let searchTerm = search;
 export let arrData = [];
 export let totalImage = 1;
-export let totalPages = null;
-totalPages = totalImage / _per_page;
+export let totalPages = 0;
+export let _per_page = 15;
+const API_KEY = '35615577-2bf6ed1d70f9a1c328f0e4a49';
+const BASE_URL = 'https://pixabay.com/api/';
+
 export async function fetchGallery(search) {
   arrData = []; // Очистка перед новым запросом
   try {
@@ -29,6 +28,7 @@ export async function fetchGallery(search) {
 
     arrData = response.data.hits;
     totalImage = response.data.totalHits;
+    totalPages = totalImage / _per_page;
     console.log(totalImage);
     console.log(arrData.length);
     console.log(response);
--- src/main.js
+++ src/main.js
@@ -3,10 +3,9 @@ import 'izitoast/dist/css/iziToast.min.css';
 import { arrData, fetchGallery, totalImage } from './js/pixabay-api';
 import { renderGallery } from './js/render-functions';
 import { totalPages } from './js/pixabay-api';
+import { _per_page } from './js/pixabay-api';
 export let search = 'cat';
 export let _page = 1;
-export let _per_page = 15;
-
 export const refs = {
   userContainerUL: document.querySelector('.users-list'),
   loader: document.querySelector('.loader'),
@@ -89,7 +88,7 @@ async function handleFormSubmit(event) {
     await fetchGallery(search); // выполняем запрос по ключевому слову
 
     if (arrData.length === 0) {
-      hideBtn;
+      hideBtn();
       iziToast.show({
         message:
           'Sorry, there are no images matching your search query. Please try again!',
@@ -106,7 +105,7 @@ async function handleFormSubmit(event) {
     } else {
       renderGallery(arrData); // отрисовываем результаты
 
-      if (arrData.length > 14) {
+      if (arrData.length >= 15) {
         showBtn();
       }
     }
@@ -127,7 +126,25 @@ refs.form.addEventListener('submit', handleFormSubmit);
 refs.btn.addEventListener('click', async () => {
   try {
     _page += 1; // увеличиваем номер страницы при клике
-
+    if (_page > totalPages) {
+      hideBtn();
+      iziToast.warning({
+        message: "We're sorry, but you've reached the end of search results.",
+        backgroundColor: '#ffa500',
+        messageSize: 16,
+        messageColor: '#FFF',
+        iconColor: '',
+        titleColor: '#fa0598e5',
+        icon: 'info-outline',
+        titleSize: 16,
+        messageLineHeight: 24,
+        position: 'topRight',
+      });
+      console.log('Скрываем кнопку');
+    } else {
+      showBtn();
+      console.log('показываем кнопку');
+    }
     await fetchGallery(search, _page); // загружаем следующую страницу
     renderGallery(arrData); // отрисовываем данные
     smoothScroll();
@@ -135,29 +152,8 @@ refs.btn.addEventListener('click', async () => {
   } catch (error) {
     console.log('Ошибка при загрузке следующей страницы:', error);
   }
-
-  // Конец страниц
-  if (_page >= totalPages) {
-    hideBtn();
-    iziToast.warning({
-      message: "We're sorry, but you've reached the end of search results.",
-      backgroundColor: '#ffa500',
-      messageSize: 16,
-      messageColor: '#FFF',
-      iconColor: '',
-      titleColor: '#fa0598e5',
-      icon: 'info-outline',
-      titleSize: 16,
-      messageLineHeight: 24,
-      position: 'topRight',
-    });
-    console.log('Скрываем кнопку');
-  } else {
-    showBtn();
-    console.log('показываем кнопку');
-  }
 });
-
+// скрол страницы
 function smoothScroll() {
   const lastArticle = refs.userContainerUL.lastElementChild;
   console.log(lastArticle);
@@ -171,3 +167,5 @@ function smoothScroll() {
     behavior: 'smooth',
   });
 }
+
+// Конец страниц
